ce5c4216088b4b40719dc81d330fdfd05001e22d
CHOI\Choi <cholnh1@naver.com>
2019-05-06 07:23:38 +0900

    info, intro 추가

--- src/main/java/com/mrporter/pomangam/common/policy/controller/PolicyViewController.java
+++ src/main/java/com/mrporter/pomangam/common/policy/controller/PolicyViewController.java
@@ -1,23 +1,37 @@
 package com.mrporter.pomangam.common.policy.controller;
 
+import com.mrporter.pomangam.common.map.service.CommonMapServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RequestMapping("/policies")
 @Controller
 @AllArgsConstructor
 public class PolicyViewController {
 
+    CommonMapServiceImpl commonMapService;
+
     @GetMapping("/privacy")
-    public String getPrivacyView(Model model) {
+    public String getPrivacyView() {
         return "policy/privacy/default";
     }
 
     @GetMapping("/terms")
-    public String getTermsView(Model model) {
+    public String getTermsView() {
         return "policy/terms/default";
+    }
+
+    @GetMapping("/intro")
+    public String getIntroView() {
+        return "policy/intro/default";
+    }
+
+    @GetMapping("/info")
+    public RedirectView getInfoView() {
+        String companyNumber = commonMapService.getValue("company-number").get(0).getValue();
+        return new RedirectView("http://www.ftc.go.kr/bizCommPop.do?wrkr_no=" + companyNumber);
     }
 }
